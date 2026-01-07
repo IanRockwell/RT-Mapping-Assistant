577887fe61b86fdf60e44676e9659d2aed163953
IanRockwell <143136750+IanRockwell@users.noreply.github.com>
2026-01-07 13:20:16 -0500

    add embed helper

---
+++ utils/embed_helper.py
@@ -0,0 +1,13 @@
+import discord
+
+EMBED_COLORS = {
+    "success": 0x57F287,
+    "warning": 0xFEE75C,
+    "error": 0xED4245,
+    "info": 0x5DADE2
+}
+
+def embed_generate(type: str, title: str, description: str = None) -> discord.Embed:
+    color = EMBED_COLORS.get(type, 0x5865F2)
+    return discord.Embed(title=title, description=description, color=color)
+
